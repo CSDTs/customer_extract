89c865c7d89bc006d39055921527502347809d2b
Kwame Porter Robinson <kporterrobinson@gmail.com>
2023-09-25 14:24:41 -0400

    (init) Replaces customer names with made up names, revises addresses to false locations, replaces busines owners name; includes unit tests

---
+++ tests/test_unit_parsing.py
@@ -0,0 +1,41 @@
+import pytest
+import pandas as pd
+from utils import localline
+
+SMALL_SAMPLE = './tests/fixture/short_sample.csv'
+
+@pytest.fixture
+def small_sample():
+    return localline.transform_data(
+        localline.load_csv(SMALL_SAMPLE)
+    )
+
+def test_number_of_customers(small_sample):
+    customers = localline.extract_customers(small_sample)
+    expected = 3
+    assert expected == len(customers),f"Unexpected number of customers, {len(customers)} vs. {expected}"
+
+@pytest.mark.skip()
+def test_multi_order():
+    pass
+
+def test_expect_one_delivery(small_sample):
+    customers = localline.extract_customers(small_sample)
+    products = localline.delivery_products(
+        a_customer=customers[0]
+    )
+
+    assert products.empty, "Expected no products to be present!"
+
+    products = localline.delivery_products(
+        a_customer=customers[1]
+    )
+
+    assert not products.empty, "Expected products to be present!"
+    assert 1 == len(products), "Expected only one (1) products to be present!"
+
+    products = localline.delivery_products(
+        a_customer=customers[2]
+    )
+
+    assert 1 == len(products), "Expected only one (1) products from person w 5 total orders!"    
---
+++ utils/localline.py
@@ -0,0 +1,82 @@
+import pandas as pd
+
+EVIDENCE_OF_BOX_DELIVERY_NEEDED = {
+    'regexes': [
+        r'\$\d+\sbox',
+        r'\$\d+\smembership'
+    ],
+    'string': [
+        "$40 box", "$100/month", "$40 membership", "Box"
+    ]
+}
+
+LOCALLINE_HEADER = "_,Order,Date,Order Status,Payment Status,Payment Method,Product,Internal Product ID,Quantity,Package Name,Product Subtotal,Product Sales Tax,Order Discount,Store Credit Applied,Fulfillment Fee,Fulfillment Tax,Total,Fulfillment Date,Fulfillment Type,Fulfillment Name,Fulfillment Address,Order Placed Time,Order Note,Backoffice Product Tags,Fulfillment Street Address,Fulfillment City,Fulfillment State,Fulfillment ZIP Code,Fulfillment Country".split(',')
+
+
+# I really wish I could make custom pyjanitor functions :(
+def _load_csv(args):
+    return pd.read_csv(**args)
+
+def _transform_data(df, args):
+    return df.dropna(**args)
+
+def load_csv(file_path):
+    args = {
+        'filepath_or_buffer': file_path,
+        'header': None
+    }
+    return _load_csv(args)
+
+def transform_data(df):
+    args = {
+        'axis': 'index',
+        'how':'all'
+    }
+    return _transform_data(df, args)
+    
+def _extract_customers(df):
+    # Create a helper column
+    df['helper'] = (df[0] == 'Grand Total').shift().fillna(0).cumsum()
+
+    # Each 'group_id' represents a different customer, so we can now group by this
+    groups = df.groupby('helper')
+
+    # Create an empty list to store our group dataframes
+    group_dfs = []
+
+    # Loop through the groups
+    for name, group in groups:
+        # Ignore the 'Grand Total' rows
+        if group[0].iloc[0] != 'Grand Total':
+            # Append the group (minus our helper column) to our list
+            group_dfs.append(group.drop('helper', axis=1))
+
+    return group_dfs
+    
+def extract_customers(df):
+    return _extract_customers(df)
+
+def delivery_products(a_customer):
+    # I should probably use nested style instead of peer hierarchry
+    # with underscore (_) to better make obvious the relationships 
+    # and support nearby modifications of code
+    def _delivery_products(a_customer, key='Product', key_index=6):
+        product_series = a_customer.iloc[:,key_index]
+        matched_rows = product_series == None # initialized with no matches
+
+        for regex in EVIDENCE_OF_BOX_DELIVERY_NEEDED['regexes']:
+            matched_rows = matched_rows | product_series.str.contains(
+                regex,
+                na=False, 
+                regex=True)
+        for a_string in EVIDENCE_OF_BOX_DELIVERY_NEEDED['string']:
+            matched_rows = matched_rows | product_series.str.contains(
+                a_string,
+                na=False, 
+                regex=False)
+
+        return a_customer[matched_rows]
+    
+    key_index = LOCALLINE_HEADER.index('Product')
+    
+    return _delivery_products(a_customer, key_index=key_index)
